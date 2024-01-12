a5a071936b8125cabb1ee4f2c438c77d09562ce6
Kreig Michaelsen <kreigmichaelsen@gmail.com>
2024-01-12 14:29:44 -0600

    Finished About Me and starting Projects

--- src/about/AboutMe.js
+++ src/about/AboutMe.js
@@ -1,13 +1,33 @@
 import { Button } from "reactstrap"
+import "./AboutMe.css"
 
 export const AboutMe = () => {
 	return (
 
         <div className="aboutMePageContainer">
+            <div className="aboutMePictureAndLinksContainer">
+            <img className="aboutMePicture" src="A89D5973-96C0-4EAE-8748-274104582D8C.JPG" width="250" height="300"></img>
+            <h2 className="aboutMeName">Kreig Michaelsen</h2>
+            <Button color="dark" className="githubButton" href="https://github.com/KreigMichaelsen" target="_blank">GitHub</Button>
+            <Button color="dark" className="websiteButton" href="https://kreigmichaelsen.github.io/portfolio-website/" target="_blank">Portfolio Website</Button>
+            </div>
 
-            <h1>About Me</h1>
-            
-            <Button>About Me!!!</Button> 
+            <div className="aboutMeTextAndTitleContainer">
+            <h1 className="aboutMeTitle">About Me</h1>
+            <p>Before joining Nashville Software School, I worked for several years in the A/V industry, where an interest in technology and programming was fostered. Being a naturally curious person, and one who enjoys puzzles, the problem-solving and analytic aspect of software development spoke to me. Also having the natural impulse for creativity, the creative nature of coding feels like a wonderful outlet for expression. I’m so very excited to continue to learn and grow in this field as a junior software developer.
+            </p>
+            <h1>Skills</h1>
+            <ul className="skillsList">
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Javascript</li>
+                <li>React</li>
+                <li>Git/GitHub</li>
+                <li>C#</li>
+                <li>.Net</li>
+
+            </ul>
+            </div>
             
         </div>
     )
--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,13 +1,37 @@
 import { Button } from "reactstrap"
 
+import "./Projects.css"
+
 export const Projects = () => {
 	return (
 
         <div className="projectPageContainer">
-
-            <h1>Projects</h1>
+            <h1 className="projectPageTitle">Projects</h1>
+            <br/>
             
-            <Button> Projects!!!</Button> 
+
+            <div className="project1Container">
+            <h2>Good As Done</h2>
+            <img src="404image" width="500" height="281"/>
+            <br/>
+            <Button color="dark" className="project1GithubButton" href="https://github.com/KreigMichaelsen/Good-As-Done---NSS-Back-End-Capstone" target="_blank">GitHub</Button>
+            
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in vodluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            </div>
+
+            <div className="project2Container">
+            <h2>Coffee Keep Up</h2>
+            <img src="404image" width="500" height="281"/>
+            <br/>
+            <Button color="dark" className="project1GithubButton" href="https://github.com/KreigMichaelsen/Coffee-Keep-Up---NSS-Front-End-Capstone" target="_blank">GitHub</Button>
+            
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            
+            </div>
+            
+            
             
         </div>
     )
