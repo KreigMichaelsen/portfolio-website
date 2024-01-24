4d8a3b2b864d86ccfd492644138e88923085a29a
Kreig Michaelsen <kreigmichaelsen@gmail.com>
2024-01-24 13:38:36 -0600

    Finished Projects Page

--- src/home/HomePage.js
+++ src/home/HomePage.js
@@ -1,5 +1,7 @@
 import { Button } from "reactstrap"
 import "./HomePage.css"
+import { AboutMe } from "../about/AboutMe"
+import { Projects } from "../projects/Projects"
 
 export const HomePage = () => {
 	return (
@@ -8,7 +10,8 @@ export const HomePage = () => {
 
             <h1 className="homePageTitle">Hi! I'm Kreig Michaelsen</h1>
             <h3 className="homePageSubtitle">I’m a software developer who loves building things and learning code</h3>
-            
+            {/* <AboutMe />
+            <Projects /> */}
            
             
         </div>
--- src/navbar/NavBar.js
+++ src/navbar/NavBar.js
@@ -1,8 +1,27 @@
 import { Link, NavLink as RRNavLink} from "react-router-dom"
 import { NavItem, Navbar, NavLink, Nav, } from "reactstrap"
 import "./NavBar.css"
+import { useEffect, useState } from "react";
 
 export const NavBar = () => {
+
+
+  // const [isNavbarFixed, setIsNavbarFixed] = useState(false);
+
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     const offset = window.scrollY;
+  //     setIsNavbarFixed(offset > 0);
+  //   };
+
+  //   window.addEventListener("scroll", handleScroll);
+
+  //   // Cleanup the event listener when the component is unmounted
+  //   return () => {
+  //     window.removeEventListener("scroll", handleScroll);
+  //   };
+  // }, []);
+
 	return (
     <Navbar className="Navbar">
         <Nav className="Nav">
@@ -24,26 +43,8 @@ export const NavBar = () => {
         </Nav>
         </Navbar>
 
-    // <div className="navbar">
-    //   <div className="navbarItemsContainer">
-    //   <li className="navbar__item">
-    //     <Link className="navbar__link" to="/home">Home</Link>
-    //   </li>
-    //   <li className="navbar__item">
-    //     <Link className="navbar__link" to="/coffeeShops">About Me</Link>
-    //   </li>
-    //   <li className="navbar__item">
-    //     <Link className="navbar__link" to="/userCoffeeShops">My Projects</Link>
-    //   </li>
-    //   <li className="navbar__item">
-    //     <Link className="navbar__link" to="/users">Connect</Link>
-    //   </li>
-    //   </div>
-    // </div>
-              
 
-
-
+            
        
     )
     
